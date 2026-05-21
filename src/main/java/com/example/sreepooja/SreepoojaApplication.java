951347dcc120d9be6201de46ab9871bbbf31fcd0
Amit-Bongale <amitbongale14@gmail.com>
2026-05-21 11:34:21 +0530

    init

---
+++ src/main/java/com/example/sreepooja/SreepoojaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sreepooja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreepoojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreepoojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sreepooja/SreepoojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sreepooja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreepoojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
